bf809d1d1abff6aaebefe8beebe45c50a1dc7857
LavanyaKrishnan <lavanyakrishnan18071998@gmail.com>
2024-12-14 21:25:50 +0530

    First Commit

---
+++ src/main/java/org/example/dockerdemoapplication/DockerDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.dockerdemoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+
+}
